d1a011812d4e432ea50fa93de3a265b0d5b6ca7d
ardi <awipratama05122006@gmail.com>
2025-10-02 16:09:41 +0800

    navigation & profile update

--- src/components/MainNavigation.tsx
+++ src/components/MainNavigation.tsx
@@ -6,28 +6,28 @@ import { RiContactsBook3Fill } from "react-icons/ri";
 export default function MainNavigation() {
     return(
         <nav className="MainNavigation w-full flex justify-end">
-            <div className="px-6 py-2 border-transparent shadow-xl rounded-2xl bg-[#F2F7FC]"> 
+            <div className="px-6 py-2 border-gray-300 border rounded-2xl bg-[#F2F7FC]"> 
                 <ul className="flex gap-6">
                     <li className="p-4 active rounded-2xl">
-                        <a href="" className="items-center flex flex-col text-[20px]">
+                        <a href="" className="items-center flex flex-col text-[18px]">
                             <FaHome />
                             Home
                         </a>
                     </li>
                     <li className="p-4 rounded-2xl">
-                        <a href="" className="items-center flex flex-col text-[20px]">
+                        <a href="" className="items-center flex flex-col text-[18px]">
                             <CgLoadbarDoc />
                             Resume
                         </a>
                     </li>
                     <li className="p-4 rounded-2xl">
-                        <a href="" className="items-center flex flex-col text-[20px]">
+                        <a href="" className="items-center flex flex-col text-[18px]">
                             <MdOutlineWorkOutline />
                             Work
                         </a>
                     </li>
                     <li className="p-4 rounded-2xl">
-                        <a href="" className="items-center flex flex-col text-[20px]">
+                        <a href="" className="items-center flex flex-col text-[18px]">
                             <RiContactsBook3Fill />
                             Contact
                         </a>
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,7 +1,10 @@
+import Ambatukam from '../assets/Ambatukam.jpg';
+
 export default function Profile() {
     return (
-        <section className="Profile w-[35%] px-4 py-10">
-            <h2 className="text-2xl font-bold">Profile</h2>
+        <section className="Profile w-[35%] px-4 py-10 flex flex-col items-center">
+            <img src={Ambatukam} alt="Profile" className="w-[50%] rounded-2xl" />
+            <h2 className="text-[38px] font-bold">Jhon Doe</h2>
             <p className="mt-4">This is the profile section.</p>
         </section>
     );
